901166cb87809f97162637dbf423688414878b3c
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2016-06-03 15:49:59 +0200

    minor update

--- src/main/scala/com/mogobiz/sample/handlers/MyCouponHandler.scala
+++ src/main/scala/com/mogobiz/sample/handlers/MyCouponHandler.scala
@@ -1,16 +1,15 @@
 package com.mogobiz.sample.handlers
 
 import com.mogobiz.run.handlers.CouponHandler
-import com.mogobiz.run.model.Mogobiz.{ReductionRule, ReductionRuleType}
+import com.mogobiz.run.model.Mogobiz.{ ReductionRule, ReductionRuleType }
 import com.mogobiz.run.model.StoreCartItemWithPrice
-
 
 class MyCouponHandler extends CouponHandler {
   override protected def applyReductionRule(rule: ReductionRule, cartItem: StoreCartItemWithPrice, quantity: Long, price: Long, totalPrice: Long, cartItems: List[StoreCartItemWithPrice]): Long = {
     rule.xtype match {
-      case ReductionRuleType.CUSTOM if rule.discount.forall(_.startsWith("MyCustomRule")) && cartItems.size > 10 =>
+      case ReductionRuleType.CUSTOM if rule.discount.forall(_.startsWith("MyCustomRule")) && cartItems.size > 10 ⇒
         computeDiscount(Some("10%"), totalPrice)
-      case _ => super.applyReductionRule(rule, cartItem, quantity, price, totalPrice, cartItems)
+      case _ ⇒ super.applyReductionRule(rule, cartItem, quantity, price, totalPrice, cartItems)
     }
   }
 }
--- src/main/scala/com/mogobiz/sample/handlers/SampleShippingHandler.scala
+++ src/main/scala/com/mogobiz/sample/handlers/SampleShippingHandler.scala
@@ -3,20 +3,18 @@ package com.mogobiz.sample.handlers
 import java.util.UUID
 
 import com.mogobiz.pay.common.Cart
-import com.mogobiz.pay.handlers.shipping.{ ShippingHandler, ShippingPrice }
-import com.mogobiz.pay.model.Mogopay.ShippingAddress
-
-import scala.collection.Seq
+import com.mogobiz.pay.handlers.shipping.ShippingHandler
+import com.mogobiz.pay.model.Mogopay.{ ShippingAddress, ShippingData }
 
 class SampleShippingHandler extends ShippingHandler {
   val SAMPLE_SHIPPING_PREFIX = "SAMPLE_SHIPPING_"
   val SAMPLE_SHIPPING_AMOUNT = 500 // in the smallest currency unit (for USD & EURO this amount is in cents)
 
-  override def computePrice(shippingAddress: ShippingAddress, cart: Cart): Seq[ShippingPrice] =
-    Seq(ShippingPrice(SAMPLE_SHIPPING_PREFIX + UUID.randomUUID().toString, UUID.randomUUID().toString, "SAMPLE", "SAMPLE", "SAMPLE", SAMPLE_SHIPPING_AMOUNT, "EUR", 2))
+  override def computePrice(shippingAddress: ShippingAddress, cart: Cart): Seq[ShippingData] =
+    Seq(ShippingData(shippingAddress.address, SAMPLE_SHIPPING_PREFIX + UUID.randomUUID().toString, UUID.randomUUID().toString, "SAMPLE", "SAMPLE", "SAMPLE", SAMPLE_SHIPPING_AMOUNT, "EUR", 2))
 
-  override def isValidShipmentId(shippingPrice: ShippingPrice): Boolean = shippingPrice.shipmentId.startsWith(SAMPLE_SHIPPING_PREFIX)
+  override def isValidShipmentId(shippingPrice: ShippingData): Boolean = shippingPrice.shipmentId.startsWith(SAMPLE_SHIPPING_PREFIX)
 
-  override def confirmShipmentId(shippingPrice: ShippingPrice): ShippingPrice = shippingPrice.copy(confirm = true)
+  override def confirmShipmentId(shippingPrice: ShippingData): ShippingData = shippingPrice.copy(confirm = true)
 
 }
